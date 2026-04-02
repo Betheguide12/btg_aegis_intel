3a19b062b67c735bac06ffbbcb14de0240fc7b09
BTG Global Mentoring & Research Academy <btheguide@gmail.com>
2026-04-02 19:10:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-import "./../styles/globals.css";
+import "../styles/globals.css";
 import React from "react";
 
 export const metadata = {
   title: "BTG AEGIS AI – Global Intelligence",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
